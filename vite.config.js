2ff11cef8dbacee878bc1b5effc8cca8df57d89c
Nyasha Shepard Ushewokunze <mrshepard18@gmail.com>
2025-02-27 16:00:51 +0200

    asset path fixed

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,22 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: '/myportfolio/',  // Replace with your repository name
+  base: '/myportfolio/',  // This is correct
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
     },
   }
 }) 
